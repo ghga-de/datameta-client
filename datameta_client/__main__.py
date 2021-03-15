00bf50e848d5a41d2d37172aaa204b0afa7aaecb
KerstenBreuer <kersten-breuer@outlook.com>
2021-03-15 18:33:06 +0100

    added state

--- datameta_client/__main__.py
+++ datameta_client/__main__.py
@@ -1,6 +1,17 @@
 import typer
+from pathlib import Path
+from typing import Optional
 
 app = typer.Typer()
+state = {
+    # container for global variables:
+    "datameta_url": None,
+    "token": None
+}
+
+@app.callback()
+def main(config:Optional[Path] = typer.Option(None)):
+    pass
 
 if __name__ == "__main__":
     app()
