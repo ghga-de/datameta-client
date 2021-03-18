5d597365bb2b2b41437bcf7ecabecc73ed263f01
KerstenBreuer <kersten-breuer@outlook.com>
2021-03-18 17:18:58 +0000

    tests don't investigate backend state

--- tests/integration/test_files.py
+++ tests/integration/test_files.py
@@ -1,29 +1,14 @@
 import os
 from datameta_client import files
 from . import fixtures
-
+from .utils import id_in_response
 
 def test_stage_file():
-    # remove any files from the datameta storage:
-    old_files = os.listdir(fixtures.storage_path)
-    for old_file in old_files:
-        os.remove(os.path.join(fixtures.storage_path, old_file))
-
     # upload file through API:
-    files.stage(
+    response = files.stage(
         name=fixtures.files[0]["name"],
         path=fixtures.files[0]["path"]
     )
+    assert id_in_response(response, has_site_id=True)
 
-    # Check if file appears in storage:
-    uploaded_files = os.listdir(fixtures.storage_path)
-    assert len(uploaded_files)==1, (
-        "No or too many files appear in the storage path"
-    )
-    uploaded_file = os.path.join(fixtures.storage_path, uploaded_files[0])
-    with open(uploaded_file, "r") as ufile:
-        uploaded_file_content = ufile.read()
-    assert uploaded_file_content==fixtures.files[0]["content"], (
-        "File contents before and after upload do not match"
-    )
 
--- tests/integration/test_metadatasets.py
+++ tests/integration/test_metadatasets.py
@@ -3,8 +3,10 @@ from datameta_client import metadatasets
 import json
 
 from . import fixtures
+from .utils import id_in_response
 
 def test_stage_metadataset():
-    metadatasets.stage(
+    response = metadatasets.stage(
         metadata_json=json.dumps(fixtures.metadataset_record)
     )
+    assert id_in_response(response, has_site_id=True)
