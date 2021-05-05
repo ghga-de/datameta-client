50198ff5fa4b22f8f4df5dcf49a18989c9648e45
Leon Kuchenbecker <leon.kuchenbecker@uni-tuebingen.de>
2021-05-05 15:38:54 +0200

    Require DCL 0.17.0

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ setup(
     },
     install_requires=[
         'typer',
-        f'datameta-client-lib>=0.14.0,<{major_version+1}',
+        f'datameta-client-lib>=0.17.0,<{major_version+1}',
         'pyyaml',
         'requests',
     ],
