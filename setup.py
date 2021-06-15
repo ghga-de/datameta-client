2f5907be3f231c97505706aa90de4a432db89372
Leon Kuchenbecker <leon.kuchenbecker@uni-tuebingen.de>
2021-06-15 16:18:03 +0200

    Bump version 0.5.0

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ from setuptools import find_packages, setup
 SETUP_DIR = os.path.dirname(__file__)
 README = os.path.join(SETUP_DIR, 'README.md')
 
-version = '0.4.0'
+version = '0.5.0'
 major_version = int(version.split(".")[0])
 
 setup(
